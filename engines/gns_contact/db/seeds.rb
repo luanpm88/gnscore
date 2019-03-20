fd52469df9520e65731643af9b3331786847b8ca
Ton Hung Nguyen <tonhungstar@gmail.com>
2019-03-20 14:05:00 +0000

    update seed data

--- engines/gns_contact/db/seeds.rb
+++ engines/gns_contact/db/seeds.rb
@@ -1,16 +1,16 @@
-GnsContact::Contact.create(full_name: 'Contact N01', email: 'contact01@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N02', email: 'contact02@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N03', email: 'contact03@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N04', email: 'contact04@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N05', email: 'contact05@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N06', email: 'contact06@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N07', email: 'contact07@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N08', email: 'contact08@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N09', email: 'contact09@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N10', email: 'contact10@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N11', email: 'contact11@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N12', email: 'contact12@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N13', email: 'contact13@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N14', email: 'contact14@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N15', email: 'contact15@demo.com')
-GnsContact::Contact.create(full_name: 'Contact N16', email: 'contact16@demo.com')
+domestic = GnsContact::Category.create(
+  name: 'Trong nước'
+) if GnsContact::Category.where(name: 'Trong nước').empty?
+
+foreign = GnsContact::Category.create(
+  name: 'Nước ngoài'
+) if GnsContact::Category.where(name: 'Nước ngoài').empty?
+
+hki = GnsContact::Contact.new(
+  code: 'HKI',
+  full_name: 'Công ty Hoàng Khang',
+  foreign_name: 'Hoang Khang Incotech',
+  contact_type: GnsContact::Contact::TYPE_COMPANY
+)
+hki.categories << domestic
+hki.save
