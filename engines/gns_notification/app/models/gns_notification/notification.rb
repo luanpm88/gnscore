1ff95bc8b2c720df0031af2e978b006bf4f745c4
Ton Hung Nguyen <tonhungstar@gmail.com>
2019-03-21 08:47:43 +0000

    update notification

--- engines/gns_notification/app/models/gns_notification/notification.rb
+++ engines/gns_notification/app/models/gns_notification/notification.rb
@@ -2,16 +2,15 @@ module GnsNotification
   class Notification < ApplicationRecord
     
     def self.add_new(phrase, user, data)
-      return 'sssss'
-			#GnsNotification::Notification::create(
-			#	phrase: phrase,
-			#	user: user.id,
-			#	data: data
-			#)
+			GnsNotification::Notification::create(
+				phrase: phrase,
+				user: user.id,
+				data: data
+			)
 		end
 
 		def get_data
-			ActiveSupport::JSON.decode(self.data)
+			ActiveSupport::JSON.decode(self.data).symbolize_keys
 		end
 		
   end
