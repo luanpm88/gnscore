bb2bd3fff47197d55f419e08320bbe215ea7631e
Ton Hung Nguyen <tonhungstar@gmail.com>
2019-03-25 04:41:07 +0000

    add name for user

--- engines/gns_core/app/models/gns_core/user.rb
+++ engines/gns_core/app/models/gns_core/user.rb
@@ -4,7 +4,7 @@ module GnsCore
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable
-           
+    
     def add_notification(phrase, data)
 			GnsNotification::Notification::create(
 				phrase: phrase,
@@ -12,5 +12,20 @@ module GnsCore
 				data: data.to_json
 			)
 		end
+    
+    # display name for user
+    def short_name
+      str = []
+      str << first_name.split(' ').last if first_name?
+      str << last_name.split(' ').first if last_name?
+      return str.join(" ")
+    end
+    
+    def full_name
+      str = []
+      str << first_name if first_name?
+      str << last_name if last_name?
+      return str.join(" ")
+    end
   end
 end
