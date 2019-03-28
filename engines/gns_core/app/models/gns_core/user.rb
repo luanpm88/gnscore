f199b214266707f0e8cd471ad751b7f13f9f16d0
Ton Hung Nguyen <tonhungstar@gmail.com>
2019-03-28 03:59:10 +0000

    fix and update user controller

--- engines/gns_core/app/models/gns_core/user.rb
+++ engines/gns_core/app/models/gns_core/user.rb
@@ -27,5 +27,31 @@ module GnsCore
       str << last_name if last_name?
       return str.join(" ")
     end
+    
+    # get select2 records
+    def self.select2(params)
+      per_page = 10
+      page = 1      
+      data = {results: [], pagination: {more: false}}
+      
+      query = self.order(:first_name)
+      
+      # keyword
+      if params[:q].present?
+        query = query.where('LOWER(gns_core_users.email) LIKE ?', '%'+params[:q].to_ascii.downcase+'%')
+      end
+      
+      # pagination
+      page = params[:page].to_i if params[:page].present?
+      query = query.limit(per_page).offset(per_page*(page-1))
+      data[:pagination][:more] = true if query.count >= per_page
+      
+      # render items
+      query.each do |user|
+        data[:results] << {id: user.id, text: user.full_name}
+      end
+      
+      return data
+    end
   end
 end
