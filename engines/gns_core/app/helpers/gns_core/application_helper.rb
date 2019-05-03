17392d966565eb8f54876f7d3f5ff7b4dac3ca4f
Ton Hung Nguyen <tonhungstar@gmail.com>
2019-05-03 03:50:04 +0000

    add display_active helper

--- engines/gns_core/app/helpers/gns_core/application_helper.rb
+++ engines/gns_core/app/helpers/gns_core/application_helper.rb
@@ -5,6 +5,14 @@ module GnsCore
       status.present? ? "<span class=\'badge badge-#{status}\'>#{t('.' + status)}</span>".html_safe : ''
     end
     
+    def display_active(active)
+      if active == true
+        "<span class=\'badge badge-active'>#{t('.active')}</span>".html_safe
+      else
+        "<span class=\'badge badge-inactive'>#{t('.inactive')}</span>".html_safe
+      end
+    end
+    
     def display_address(object)
       str = []
       str << object.address if object.address.present?
