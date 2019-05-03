c1c1bb11bfdab0d7f9de590d9f244b612c00c802
Ton Hung Nguyen <tonhungstar@gmail.com>
2019-05-03 02:22:17 +0000

    update display_address helper

--- engines/gns_core/app/helpers/gns_core/application_helper.rb
+++ engines/gns_core/app/helpers/gns_core/application_helper.rb
@@ -4,5 +4,14 @@ module GnsCore
     def status_badge(status)
       status.present? ? "<span class=\'badge badge-#{status}\'>#{t('.' + status)}</span>".html_safe : ''
     end
+    
+    def display_address(object)
+      str = []
+      str << object.address if object.address.present?
+      str << object.district_name if object.district_name.present?
+      str << object.state_name if object.state_name.present?
+      str << object.country_name if object.country_name.present?
+      return str.join(", ")
+    end
   end
 end
